69db8283dfa2787cffe4a0af92310e08e5f10a2a
GeniaBarykin <467232@student.saxion.nl>
2024-04-14 01:19:55 +0300

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Navbar } from './pages/Navbar';
 import { About } from './pages/About';
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="App">
      <QueryClientProvider client={client}>
-      <Router>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/about" element={<About/>}/>
           <Route path="*" element={<Home/>}/>
         </Routes>
-      </Router>
+      </HashRouter>
      </QueryClientProvider>
     </div>
   );
