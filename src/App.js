13a7df3cd42e6bc384ead889447bf968f775125f
GeniaBarykin <467232@student.saxion.nl>
2024-04-13 23:19:14 +0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Navbar } from './pages/Navbar';
 import { About } from './pages/About';
